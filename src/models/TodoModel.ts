5dfafe9eacafa74dcbd30bc46e49b2c93f05bcb4
Jean Meira <jean.meira10@hotmail.com>
2023-02-10 22:09:24 -0300

    feat: add todo model

---
+++ src/models/TodoModel.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const TodoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: false },
+  priority: { type: String, required: false },
+  created_by: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "user",
+  },
+  date: { type: Number, required: false },
+  created_at: { type: Number, required: true },
+  updated_at: { type: Number, required: true },
+});
+
+const TodoModel = mongoose.model("todo", TodoSchema);
+
+export default TodoModel;
