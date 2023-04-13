693bec341981cd7a5ed551bb7a5646272aec80bf
Jean Meira <jean.meira10@hotmail.com>
2023-04-13 15:08:15 -0300

    feat: add teste route

--- src/routes/globalRoutes.ts
+++ src/routes/globalRoutes.ts
@@ -3,6 +3,9 @@ import UserController from "../controllers/UserController";
 
 const globalRoutes = express.Router();
 
+globalRoutes.get("/teste", (req, res) =>
+  res.json([{ title: "todo 1", isCompleted: false }])
+);
 globalRoutes.post("/user/create", UserController.createUser);
 globalRoutes.post("/user/login", UserController.Login);
 
