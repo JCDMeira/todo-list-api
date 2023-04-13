58608ec315764930b33186d6b55b13a844b5201b
Jean Meira <jean.meira10@hotmail.com>
2023-04-13 15:08:06 -0300

    feat: apply cors policy

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import routes from "./routes";
 import "./config/dbConfig";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 routes(app);
